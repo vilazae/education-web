d4ee67b0feb624018802e30f606c6448ab45a707
ARSYSLAN\vmlazaro <vmlazaro@arsys.es>
2019-01-24 17:46:42 +0100

    Last changes done

--- app/app.js
+++ app/app.js
@@ -6,10 +6,10 @@ var educationApp = angular.module('educationApp', ['ui.router', 'ngCookies']);
 /**
  * App configuration.
  */
-educationApp.config([ '$stateProvider', '$urlRouterProvider', '$httpProvider', '$cookiesProvider', 
+educationApp.config([ '$stateProvider', '$urlRouterProvider', '$httpProvider', '$cookiesProvider',
 	function($stateProvider, $urlRouterProvider, $httpProvider, $cookiesProvider) {
 
-		
+
 		$urlRouterProvider.otherwise('/inicio');
 
 		$stateProvider
@@ -22,26 +22,35 @@ educationApp.config([ '$stateProvider', '$urlRouterProvider', '$httpProvider', '
 			url: "/variables",
 			templateUrl: "app/templates/variables.html",
 			controller: "variablesCtrl as varsCtrl"
-		})		
+		})
 		.state('conditionals', {
 			url: "/condicionales",
 			templateUrl: "app/templates/conditionals.html",
 			controller: "conditionalsCtrl as condCtrl"
-		})		
+		})
 		.state('exercises', {
 			url: "/ejercicios",
 			templateUrl: "app/templates/exercises.html",
 			controller: "exercisesCtrl as exCtrl"
 		})
 
-		
-			
+
+
 	}
 ]);
 
+
+educationApp.run( [ "$rootScope", "$state", "$stateParams", "$anchorScroll",
+	function ($rootScope, $state, $stateParams, $anchorScroll) {
+
+		$rootScope.$on('$stateChangeStart', function () {
+			$anchorScroll();
+		} );
+	}
+] );
 educationApp.controller('homeController', [ function( apiService ) {
 
-	console.log('ctrl saludo homeController!');	
+	console.log('ctrl saludo homeController!');
 	var me = this;
 	me.nombre = window.nombreUsuario;
 	me.saludo = 'hola men!';
@@ -49,7 +58,7 @@ educationApp.controller('homeController', [ function( apiService ) {
 
 educationApp.controller('variablesCtrl', [ function( apiService ) {
 
-	console.log('ctrl saludo variablesCtrl!');	
+	console.log('ctrl saludo variablesCtrl!');
 	var me = this;
 	  me.list1 = {title: 'AngularJS - Drag Me'};
   me.list2 = {};
@@ -58,15 +67,38 @@ educationApp.controller('variablesCtrl', [ function( apiService ) {
 } ] );
 educationApp.controller('conditionalsCtrl', [ function( apiService ) {
 
-	console.log('ctrl saludo conditionalsCtrl!');	
+	console.log('ctrl saludo conditionalsCtrl!');
 	var me = this;
 	me.nombre = window.nombreUsuario;
 	me.saludo = 'hola men!';
 } ] );
-educationApp.controller('exercisesCtrl', [ function( apiService ) {
+educationApp.controller('exercisesCtrl', [ "$anchorScroll", function( $anchorScroll ) {
 
-	console.log('ctrl saludo exercisesCtrl!');	
+
 	var me = this;
-	me.nombre = window.nombreUsuario;
-	me.saludo = 'hola men!';
+
+	me.evaluateTest = function () {
+		me.resolved = true;
+		me.errorEx1      = false;
+		me.errorEx2      = false;
+		me.errorEx3      = false;
+		me.hasError = false;
+
+		if ( me.ex1 !== '18' ) {
+			me.errorEx1 = true;
+			me.hasError = true;
+
+		}
+		if ( me.ex2 !== 'Fonseca' ) {
+			me.errorEx2 = true;
+			me.hasError = true;
+		}
+		if ( me.ex3 !== '12' ) {
+			me.errorEx3 = true;
+			me.hasError = true;
+		}
+		$anchorScroll();
+
+	}
+
 } ] );
